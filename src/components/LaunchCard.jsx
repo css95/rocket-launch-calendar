d2d249bfb0db3f227466891ffdb870858d92bf7f
css95 <steilner.carolin@gmail.com>
2026-04-22 21:52:44 +0200

    style: format launch dates to en-GB

--- src/components/LaunchCard.jsx
+++ src/components/LaunchCard.jsx
@@ -2,11 +2,19 @@ import './LaunchCard.css'
 
 function LaunchCard({ launch }) {
 
+    const launchDate = new Date(launch.net).toLocaleString('en-GB', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit'
+                    })
+
     return (
         <div className="launch-card">
             <div className="launch-card__info">
                 <h2>{launch.name}</h2>
-                    <p className="launch-card__date">{launch.net}</p>
+                    <p className="launch-card__date">{launchDate}</p>
                     <p className="launch-card__status">{launch.status?.name}</p>
                     <p className="launch-card__description">{launch.mission?.description}</p>
                     <p className="launch-card__location">{launch.pad?.location?.name}</p>
